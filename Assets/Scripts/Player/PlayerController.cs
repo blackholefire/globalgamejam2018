366c0e734d0b0b8a84a68c7ffee263321d7b55d2
Bowden <rwbowden@labs.neit.edu>
2018-01-26 17:32:51 -0500

    first commit

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float h = speed * Input.GetAxis("Horizontal");
+        float v = speed * Input.GetAxis("Vertical");
+
+        Vector3 movment = new Vector3(h, 0.0f, v);
+
+        transform.Translate(movment);
+    }
+}
