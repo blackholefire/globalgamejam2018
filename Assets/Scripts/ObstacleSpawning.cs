6d8cf1ab9c8452f5252cdb7e30652fa2c935a0ea
Bowden <rwbowden@labs.neit.edu>
2018-01-26 21:55:51 -0500

    More stuff

---
+++ Assets/Scripts/ObstacleSpawning.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawning : MonoBehaviour {
+
+    public GameObject floor;
+    public GameObject block;
+
+    public int obstacleCount = 10;
+
+    // Use this for initialization
+    void Start () {
+        Renderer rend = GetComponent<Renderer>();
+
+        Vector3 offset = transform.position - transform.forward * rend.bounds.max.z;
+        Vector3 pos = transform.position + offset;
+
+        pos.y = 0.5f;
+        for (int i = 0; i < obstacleCount; i++)
+        {
+            pos.x = Random.Range(rend.bounds.min.x, rend.bounds.max.x);
+
+            Instantiate(block, pos, Quaternion.identity);
+            pos.z += 15;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+           print( GetComponent<Renderer>().bounds.max.z);
+            Vector3 newPos = new Vector3(transform.position.x, transform.position.y, transform.position.z +150);
+            Instantiate(floor, newPos, Quaternion.identity);
+        }
+	}
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,19 +4,60 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     public float speed = 2.0f;
+    private Vector3 jump;
+    public float jumpForce = 2.0f;
+    public float dashSpeed = 4;
 
+    bool isGrounded;
+
+    Rigidbody rb;
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
 	}
 	
 	// Update is called once per frame
-	void FixedUpdate () {
+	void FixedUpdate ()
+    {
         float h = speed * Input.GetAxis("Horizontal");
         float v = speed * Input.GetAxis("Vertical");
 
-        Vector3 movment = new Vector3(h, 0.0f, v);
+        Vector3 movement = new Vector3(h, 0.0f, v);
 
-        transform.Translate(movment);
+        transform.Translate(movement);
+
+        if (!isGrounded && rb.velocity.y == 0)
+        {
+            isGrounded = true;
+        }
+
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+        if (Input.GetButtonDown("Dash")) Dash();
     }
+
+    void Dash()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        //movement = Vector3.ClampMagnitude(movement, 1);
+
+        transform.position = transform.position + movement * dashSpeed;
+
+        if(transform.position.x > 4.3f)
+        {
+            transform.position = new Vector3(4.0f, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < -4.3f)
+        {
+            transform.position = new Vector3(-4.0f, transform.position.y, transform.position.z);
+        }
+
+    }
+
 }
